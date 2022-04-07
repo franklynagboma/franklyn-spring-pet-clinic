edf7771f40c7c87c0c89cb94e6e44ba3b27d5c9c
frankynagboma <franklyn1great@gmail.com>
2022-04-08 00:52:54 +0100

    List all Vets on Vet index page. Closes #15

--- pet-clinic-web/src/main/java/com/dev/franklynspringpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/dev/franklynspringpetclinic/controllers/VetController.java
@@ -3,15 +3,23 @@
  */
 package com.dev.franklynspringpetclinic.controllers;
 
+import com.dev.franklynspringpetclinic.service.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
-    @RequestMapping(value = {"/vets", "/vets/index", "/vets/index.html"})
-    public String modelADisplayListOfVet() {
+    private final VetService vetService;
 
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping(value = {"/vets", "/vets/index", "/vets/index.html"})
+    public String modelADisplayListOfVet(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }

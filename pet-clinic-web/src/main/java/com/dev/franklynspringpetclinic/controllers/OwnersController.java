a58702f06eaf964c0ba22f49f11382e557123638
frankynagboma <franklyn1great@gmail.com>
2022-04-08 00:41:12 +0100

    List all Owners on Owner index page. Closes #16

--- pet-clinic-web/src/main/java/com/dev/franklynspringpetclinic/controllers/OwnersController.java
+++ pet-clinic-web/src/main/java/com/dev/franklynspringpetclinic/controllers/OwnersController.java
@@ -3,21 +3,29 @@
  */
 package com.dev.franklynspringpetclinic.controllers;
 
+import com.dev.franklynspringpetclinic.service.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-// Since currently everything in this controller(Owners) mapps to owner,
+// Since currently everything in this controller(Owners) maps to owner,
 // it can be prefix like below
 @RequestMapping(value = "/owners")
 @Controller
 public class OwnersController {
 
+    private final OwnerService ownerService;
+
+    public OwnersController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     // empty space need for when just prefix, it work well
     // without empty since spring 5 but just to make sure no
     // assumption.
     @RequestMapping(value = {"", "/", "/index", "/index.html"})
-    public String modelDisplayListOfOwners() {
-
+    public String modelDisplayListOfOwners(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }

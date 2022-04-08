f6b17ae9c3639380669c9eda385dc08ce1f6e751
frankynagboma <franklyn1great@gmail.com>
2022-04-08 11:34:57 +0100

    Update Map impl to manage setting of the ID property. Closes #23

--- pet-clinic-data/src/main/java/com/dev/franklynspringpetclinic/service/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/com/dev/franklynspringpetclinic/service/map/AbstractMapService.java
@@ -3,14 +3,13 @@
  */
 package com.dev.franklynspringpetclinic.service.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import com.dev.franklynspringpetclinic.model.BaseEntity;
 
-public abstract class AbstractMapService<T, ID> {
+import java.util.*;
 
-    protected Map<ID, T> map = new HashMap<>();
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
+
+    protected Map<Long, T> map = new HashMap<>();
 
     Set<T> findAll() {
         return new HashSet<>(map.values());
@@ -20,8 +19,15 @@ public abstract class AbstractMapService<T, ID> {
         return map.get(id);
     }
 
-    T save(ID id, T object) {
-        map.put(id, object);
+    T save(T object) {
+        if (object != null) {
+            if (object.getId() == null) {
+                object.setId(getNextId());
+            }
+            map.put(object.getId(), object);
+        } else {
+            throw new NullPointerException("Object cannot be null");
+        }
         return object;
     }
 
@@ -33,5 +39,15 @@ public abstract class AbstractMapService<T, ID> {
         map.entrySet().removeIf(entry ->
                 entry.getValue().equals(object)
         );
+    }
+
+    private Long getNextId() {
+        long nextId;
+        if (map.isEmpty()) {
+            nextId = 1L;
+        }else  {
+            nextId = Collections.max(map.keySet()) + 1;
+        }
+        return nextId;
     }
 }
--- pet-clinic-data/src/main/java/com/dev/franklynspringpetclinic/service/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/com/dev/franklynspringpetclinic/service/map/VetServiceMap.java
@@ -20,7 +20,7 @@ public class VetServiceMap
 
     @Override
     public Vet save(Vet object) {
-        return super.save(object.getId(), object);
+        return super.save(object);
     }
 
     @Override
